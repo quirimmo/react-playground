c6051a11b9e849647522b5d026242c97249476b6
quirimmo <quirimmo@gmail.com>
2018-01-10 22:56:42 +0000

    played with routes and react

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,37 +1,35 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 
-import AwesomeComponent from './AwesomeComponent.jsx';
-import TodoListComponent from './TodoListComponent.jsx';
+import AwesomeComponent from './AwesomeComponent.jsx'
+import TodoListComponent from './TodoListComponent.jsx'
 
-import css from './../../assets/styles/main.css';
+import css from './../../assets/styles/main.css'
 
 class App extends Component {
-	  
-	render () {
-    	return (
-			<div>
-				<p>Hello React Project</p>
-				<br/>
-				<AwesomeComponent />
-				<br/>
-				<TodoListComponent />
-				<br/>
-				<BrowserRouter>
-					<div>
-						<Link to='/awesome-component'> Awesome Component </Link>
-						<Link to='/todo'> Todo List </Link>
-						<Switch>
-							<Route path='/awesome-component' component={AwesomeComponent} />
-							<Route path='/todo' component={TodoListComponent} />
-						</Switch>
-					</div>
-				</BrowserRouter>
-			</div>
-    	);
-	}
-
+  render () {
+    return (
+      <div>
+        <p>Hello React Project</p>
+        <br />
+        <AwesomeComponent />
+        <br />
+        <TodoListComponent />
+        <br />
+        <BrowserRouter>
+          <div>
+            <Link to='/awesome-component'> Awesome Component </Link>
+            <Link to='/todo'> Todo List </Link>
+            <Switch>
+              <Route path='/awesome-component' component={AwesomeComponent} />
+              <Route path='/todo' component={TodoListComponent} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
