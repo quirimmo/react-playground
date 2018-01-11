54166d492c0e3c8c1ebc1f200f42fabb8259be85
quirimmo <quirimmo@gmail.com>
2018-01-11 20:46:27 +0000

    added react semantic ui

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,35 +1,41 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 
-import AwesomeComponent from './AwesomeComponent.jsx'
-import TodoListComponent from './TodoListComponent.jsx'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
-import css from './../../assets/styles/main.css'
+import AwesomeComponent from './AwesomeComponent.jsx';
+import TodoListComponent from './TodoListComponent.jsx';
+
+import css from './../../assets/styles/main.css';
 
 class App extends Component {
-  render () {
-    return (
-      <div>
-        <p>Hello React Project</p>
-        <br />
-        <AwesomeComponent />
-        <br />
-        <TodoListComponent />
-        <br />
-        <BrowserRouter>
-          <div>
-            <Link to='/awesome-component'> Awesome Component </Link>
-            <Link to='/todo'> Todo List </Link>
-            <Switch>
-              <Route path='/awesome-component' component={AwesomeComponent} />
-              <Route path='/todo' component={TodoListComponent} />
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </div>
-    )
-  }
+	render () {
+		return (
+			<MuiThemeProvider>
+				<div>
+					<RaisedButton label="Default" />
+					<p>Hello React Project</p>
+					<br/>
+					<AwesomeComponent />
+					<br/>
+					<TodoListComponent />
+					<br/>
+					<BrowserRouter>
+						<div>
+							<Link to='/awesome-component'>Awesome Component</Link>
+							<Link to='/todo'>Todo List</Link>
+							<Switch>
+								<Route path='/awesome-component' component={AwesomeComponent} />
+								<Route path='/todo' component={TodoListComponent} />
+							</Switch>
+						</div>
+					</BrowserRouter>
+				</div>
+			</MuiThemeProvider>
+    	)
+  	}
 }
 
-export default App
+export default App;
