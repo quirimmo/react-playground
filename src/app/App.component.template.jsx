905c35cac309cb69085d7ecec2ab532f423f2dfa
quirimmo <quirimmo@gmail.com>
2018-01-13 21:41:13 +0000

    added react version

--- src/app/App.component.template.jsx
+++ src/app/App.component.template.jsx
@@ -1,6 +1,8 @@
+'use strict';
+
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import { HashRouter , BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -10,10 +12,38 @@ import TodoListComponent from './TodoListComponent.jsx';
 
 import css from './../../assets/styles/main.scss';
 
+import { BarChart } from 'react-d3-components';
+
+
 function getTemplate() {
+
+    var tooltip = function(x, y0, y, total) {
+            return y.toString();
+    };
+
+	var data = [{
+		label: 'somethingA',
+		values: [
+			{ x: 'SomethingA', y: 10 },
+			{ x: 'SomethingB', y: 4 },
+			{ x: 'SomethingC', y: 3 },
+			{ x: 'SomethingD', y: -3 }
+		]
+	}];
+
+
     return (
         <MuiThemeProvider>
             <div>
+                <BarChart
+                    data={data}
+                    width={800}
+                    height={400}
+                    margin={{top: 10, bottom: 50, left: 50, right: 10}}
+                    tooltipHtml={tooltip}
+                    colorByLabel={false}
+                />,
+                <br/>
                 <RaisedButton label="Default" />
                 <p>Hello React Project</p>
                 <br/>
@@ -22,7 +52,7 @@ function getTemplate() {
                 <TodoListComponent />
                 <br/>
                 <div>{this.createMenuItems()}</div>
-                <BrowserRouter>
+                <HashRouter>
                     <div>
                         <Link to='/awesome-component'>Awesome Component</Link>
                         <Link to='/todo'>Todo List</Link>
@@ -31,7 +61,7 @@ function getTemplate() {
                             <Route path='/todo' component={TodoListComponent} />
                         </Switch>
                     </div>
-                </BrowserRouter>
+                </HashRouter>
             </div>
         </MuiThemeProvider>
     );
