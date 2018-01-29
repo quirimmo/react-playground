20baa9d0e6028f4937e819006732d96209a1fd94
Quirino Lo Russo <quirino.x.lo.russo@aib.ie>
2018-01-29 15:43:36 +0000

    starting with the creation of the main menu of the app

--- src/app/App.component.template.jsx
+++ src/app/App.component.template.jsx
@@ -10,25 +10,7 @@ import { BarChart } from 'react-d3-components';
 
 import css from './../../assets/styles/main.scss';
 
-
-
 function getTemplate() {
-
-    var tooltip = function(x, y0, y, total) {
-		return y.toString();
-    };
-
-	var data = [{
-		label: 'somethingA',
-		values: [
-			{ x: 'A', y: 10 },
-			{ x: 'B', y: 4 },
-			{ x: 'C', y: 3 },
-			{ x: 'D', y: -3 }
-		]
-	}];
-
-
     return (
         <MuiThemeProvider>
             <div>
@@ -46,6 +28,21 @@ function getTemplate() {
 }
 
 export default getTemplate;
+
+
+// var tooltip = function(x, y0, y, total) {
+//     return y.toString();
+// };
+
+// var data = [{
+//     label: 'somethingA',
+//     values: [
+//         { x: 'A', y: 10 },
+//         { x: 'B', y: 4 },
+//         { x: 'C', y: 3 },
+//         { x: 'D', y: -3 }
+//     ]
+// }];
 
 
 // <MuiThemeProvider>
