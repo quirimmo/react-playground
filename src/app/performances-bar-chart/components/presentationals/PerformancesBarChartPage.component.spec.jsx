4042f9b438a5707c41661418fd5fe11f98f0d8d3
quirimmo <quirimmo@gmail.com>
2018-02-16 14:56:01 +0000

    finished unit tests per Performances Chart Components

--- src/app/performances-bar-chart/components/presentationals/PerformancesBarChartPage.component.spec.jsx
+++ src/app/performances-bar-chart/components/presentationals/PerformancesBarChartPage.component.spec.jsx
@@ -3,7 +3,7 @@ import PerformancesBarChart from './PerformancesBarChart.component.jsx';
 import PerformancesModel from './../../model/Performances.model.jsx';
 import PerformancesBarChartPage from './PerformancesBarChartPage.component.jsx';
 
-describe.only('PerformancesBarChartPage', () => {
+describe('PerformancesBarChartPage', () => {
     const wrapper = shallow(<PerformancesBarChartPage />);
 
     describe('general', () => {
@@ -18,38 +18,93 @@ describe.only('PerformancesBarChartPage', () => {
                 wrapper.instance().setupChartData.should.be.a('Function');
             });
 
-            it('should instantiate the PerformancesModel', () => {
-                PerformancesModel.constructor = spy();
-                wrapper.instance().setupChartData();
-                PerformancesModel.constructor.should.have.been.called();
-                // wrapper.instance().setupChartData.should.be.a('Function');
+            it('should define the chartData property', () => {
+                wrapper.instance().chartData.should.be.an('Object');
+                wrapper.instance().chartData.scenariosChartData.should.be.an('Array');
+                wrapper.instance().chartData.stepsChartData.should.be.an('Array');
+            });
+        });
+
+        describe('_getScenariosTooltip', () => {
+            it('should be a function', () => {
+                wrapper.instance()._getScenariosTooltip.should.be.a('Function');
             });
 
-            // it('should return the right HTML', () => {
-            //     wrapper.instance()._getDefaultTooltip(null, null, 'Hello World', null).should.be.eql(
-            //         <div>
-            //             <h1>Hello World</h1>
-            //         </div>
-            //     );
-            // });
+            describe('HTML', () => {
+                const scenarioTooltipLayout = shallow(wrapper.instance()._getScenariosTooltip(1, null, null, 'Hello World', null));
+
+                describe('container', () => {
+                    it('should present the class name', () => {
+                        scenarioTooltipLayout.find('div').hasClass('wrapper-scenario-chart-tooltip').should.be.true;
+                    });
+                });
+
+                describe('title', () => {
+                    it('should be present', () => {
+                        scenarioTooltipLayout.find('h3').should.not.be.null;
+                    });
+                });
+
+                describe('subtitle', () => {
+                    it('should be present', () => {
+                        scenarioTooltipLayout.find('h4').should.not.be.null;
+                    });
+
+                    it('should present the class name', () => {
+                        scenarioTooltipLayout.find('h4').hasClass('single-scenario-file').should.be.true;
+                    });
+                });
+
+                describe('duration', () => {
+                    it('should be present', () => {
+                        scenarioTooltipLayout.find('h5').should.not.be.null;
+                    });
+
+                    it('should present the class name', () => {
+                        scenarioTooltipLayout.find('h5').hasClass('single-scenario-duration').should.be.true;
+                    });
+                });
+            });
+        });
+
+        describe('_getStepsTooltip', () => {
+            it('should be a function', () => {
+                wrapper.instance()._getStepsTooltip.should.be.a('Function');
+            });
+
+            describe('HTML', () => {
+                const stepTooltipLayout = shallow(wrapper.instance()._getStepsTooltip(1, null, null, 'Hello World', null));
+
+                describe('container', () => {
+                    it('should present the class name', () => {
+                        stepTooltipLayout.find('div').hasClass('wrapper-scenario-chart-tooltip').should.be.true;
+                    });
+                });
+
+                describe('title', () => {
+                    it('should be present', () => {
+                        stepTooltipLayout.find('h3').should.not.be.null;
+                    });
+                });
+
+                describe('duration', () => {
+                    it('should be present', () => {
+                        stepTooltipLayout.find('h5').should.not.be.null;
+                    });
+
+                    it('should present the class name', () => {
+                        stepTooltipLayout.find('h5').hasClass('single-scenario-duration').should.be.true;
+                    });
+                });
+            });
         });
     });
 
-    // describe('HTML', () => {
-    //     describe('container', () => {
-    //         it('should has the right class name', () => {
-    //             wrapper.find('div').hasClass('wrapper-performances-bar-chart').should.be.true;
-    //         });
-    //     });
-
-    //     describe('BarChart', () => {
-    //         it('should be defined', () => {
-    //             wrapper.find(BarChart).should.not.be.null;
-    //         });
-
-    //         it('should pass the data as props of the component', () => {
-    //             wrapper.find(BarChart).props().data.should.be.eql(data);
-    //         });
-    //     });
-    // });
+    describe('HTML', () => {
+        describe('PerformancesBarChart', () => {
+            it('should instantiate two PerformancesBarChart', () => {
+                wrapper.find(PerformancesBarChart).length.should.be.eql(2);
+            });
+        });
+    });
 });
