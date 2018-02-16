2b342e8fbc695dc0ab43dac5af3cfb89814bcc96
quirimmo <quirimmo@gmail.com>
2018-02-16 15:12:06 +0000

    added performances model unit tests

--- src/app/performances-bar-chart/model/Performances.model.jsx
+++ src/app/performances-bar-chart/model/Performances.model.jsx
@@ -1,23 +1,21 @@
-import statistics from '../../../../data/statistics.json';
+import statistics from '../../../../data/statistics.json'
 
 class PerformancesModel {
+	constructor () {
+    	this.totalDuration = +(statistics.duration / 1000).toFixed(2)
+		this.scenarios = statistics.scenarios
+		this.scenarios.forEach(roundScenarioDuration.bind(this))
+		this.steps = statistics.steps
+		this.steps.forEach(roundStepDuration.bind(this))
 
-    constructor() {
-        this.totalDuration = (statistics.duration / 1000).toFixed(2);
-        this.scenarios = statistics.scenarios;
-        this.scenarios.forEach(roundScenarioDuration.bind(this));
-        this.steps = statistics.steps;
-        this.steps.forEach(roundStepDuration.bind(this));
+		function roundScenarioDuration (scenario) {
+			scenario.duration = +(scenario.duration / 1000).toFixed(2)
+		}
 
-        function roundScenarioDuration(scenario) {
-            scenario.duration = + (scenario.duration / 1000).toFixed(2);
-        }
-
-        function roundStepDuration(step) {
-            step.duration = + (step.duration / 1000).toFixed(2);
-        }
-    }
-
+		function roundStepDuration (step) {
+			step.duration = +(step.duration / 1000).toFixed(2)
+		}
+  	}
 }
 
-export default PerformancesModel = PerformancesModel;
+export default (PerformancesModel = PerformancesModel);
